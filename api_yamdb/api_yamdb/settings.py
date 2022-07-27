a68576e268cd753862be08e1242c0e3ce2c85eae
Iliya Steblyan <stebljan@ya.ru>
2022-07-27 07:46:45 +0600

    Добавил модель пользователя

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -85,6 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+CHOICES = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    """Кастомизирует пользовательский класс."""
+
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        'Пользовательская роль',
+        max_length=16,
+        choices=CHOICES,
+        default='user'
+    )
