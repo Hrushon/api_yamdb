18c5d6374ca08b347c5d490f0289286afd735a4b
DamirKasimov <d:\mail>
2022-07-28 14:38:45 +0400

    models Categories, Genres, Titles done

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import sqlite3
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    category = models.ForeignKey(Categories, related_name='titles', on_delete=models.PROTECT)
+    genres = models.ManyToManyField(Genres, through='GenresTitle')
+    description = models.TextField(null=True)
+
+    def get_genres(self):
+        return list(self.genres.all())
+
+    def __str__(self):
+        return f'{self.name} {self.genres}'
+
+
+class GenresTitle(models.Model):
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'id {self.pk} жанр {self.genre}  title  {self.title}'
